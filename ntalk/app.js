52fa8dd796e83c0e5af425a0143490bfa04a8f70
Felipe Mendes Neves <felipemen74@gmail.com>
2025-06-29 22:00:38 -0300

    Controlando as seções de usuarios

--- ntalk/app.js
+++ ntalk/app.js
@@ -1,10 +1,17 @@
 var express = require('express');
 var load = require('express-load');
+var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
+var expressSession = require('express-session');
 
 var app = express();
 
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
+app.use(cookieParser('ntalk'));
+app.use(expressSession({secret: 'ntalk', resave: true, saveUninitialized: true}));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(__dirname + '/public'));
 
 load('models')
