cdabee8a032ac3e30509a406e105134032111a7c
Felipe Mendes Neves <felipemen74@gmail.com>
2025-07-05 16:47:34 -0300

    manipulação de dados com socket.io

--- ntalk/app.js
+++ ntalk/app.js
@@ -1,3 +1,4 @@
+const KEY = 'ntalk.sid', SECRET = 'ntalk';
 var express = require('express');
 var load = require('express-load');
 var bodyParser = require('body-parser');
@@ -10,28 +11,40 @@ var app = express();
 
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
+var cookie = cookieParser(SECRET);
+var store = new expressSession.MemoryStore();
 
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 app.use(cookieParser('ntalk'));
-app.use(expressSession({secret: 'ntalk', resave: true, saveUninitialized: true}));
+app.use(expressSession({secret: SECRET, name: KEY, store: store, resave: true, saveUninitialized: true}));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
 app.use(express.static(__dirname + '/public'));
+
+io.use(function(socket, next) {
+  var data = socket.request;
+  cookie(data, {}, function(err) {
+    var sessionID = data.signedCookies[KEY];
+    store.get(sessionID, function(err, session) {
+      if(err || !session) {
+        return next(new Error('unauthorized'));
+      } else {
+        socket.handshake.session = session;
+        return next();
+      }
+    });
+  });
+});
 
 load('models')
   .then('controllers')
   .then('routes')
   .into(app);
 
-io.sockets.on('connection', function (client) {
-  client.on('send-server', function (data) {
-    var msg = "<b>"+data.nome+":</b> "+data.msg+"<br>";
-    client.emit('send-client', msg);
-    client.broadcast.emit('send-client', msg);
-  });
-});
+load('sockets')
+  .into(io);
 
 app.use(error.notFound);
 app.use(error.serverError);
