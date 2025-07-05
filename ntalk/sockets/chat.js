034e883a7963caa0f354c4309970a3a457f5194d
Felipe Mendes Neves <felipemen74@gmail.com>
2025-07-05 16:59:44 -0300

    Pegando dados de usuario na sessão

--- ntalk/sockets/chat.js
+++ ntalk/sockets/chat.js
@@ -1,9 +1,11 @@
 module.exports = function(io) {
     var sockets = io.sockets;
     sockets.on('connection', function(client) {
-        client.on('send-server', function(data) {
-            var msg = "<b>"+data.nome+":</b> "+data.msg+"<br>";
-            sockets.emit('send-client', msg);
+        var session = client.handshake.session;
+        var usuario = session.usuario;
+        client.on('send-server', function(msg) {
+            msg = "<b>"+ usuario.nome +":</b> "+ msg +"<br>";
+            client.emit('send-client', msg);
             client.broadcast.emit('send-client', msg);
         });
     });
